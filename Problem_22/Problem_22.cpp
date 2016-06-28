fccd98d509218a7a694d78f14a1616a76d5c4483
Platon-Nikolaos Kiorpelidis <platwnace@gmail.com>
2016-06-28 22:29:10 +0300

    added solution to problem 22

--- Problem_22/Problem_22.cpp
+++ Problem_22/Problem_22.cpp
@@ -8,6 +8,7 @@ Language: C++
 #include <iostream>
 #include <cstdint>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,37 +21,56 @@ int main()
 
   file >> file_names;
 
-  //DEBUG
-  cout << file_names;
-
   uint32_t num_words = 0;
   uint32_t i = 0;
   while(i < file_names.length())
   {
     if(file_names[i] == ',')
     {
-      file_names[i] = '\n';
+      num_words++;
     }
     i++;
   }
+  num_words++;
 
-  cout << "num_words :" << num_words << endl;
+  string *names = NULL;
+  names = new string[num_words];
 
-  file_names.append("\n");
-  cout << file_names;
-
+  uint32_t counter = 0;
   i = 0;
-  uint32_t counter = 1;
   while(i < file_names.length())
   {
-    cout << file_names[i];
-    if(file_names[i] == '\n')
+    if(file_names[i] != '\"' && file_names[i] != ',')
     {
-      cout << counter;
+      names[counter].append(1, file_names[i]);
+    }
+    else if(file_names[i] == ',')
+    {
+      i++;
       counter++;
     }
     i++;
   }
+
+  vector<string> my_vector(names, names + num_words);
+
+  sort(my_vector.begin(), my_vector.begin()+num_words);
+
+
+  uint32_t name_value = 0;
+  uint32_t all_names_value = 0;
+  vector<string>::iterator it=my_vector.begin();
+  for(i = 0; i < my_vector.size(); i++)
+  {
+    name_value = 0;
+    for(size_t j = 0; j < it[i].length(); j++)
+    {
+      name_value += it[i][j] - 64;
+    }
+    name_value *= (i + 1);
+    all_names_value += name_value;
+  }
+  cout << "ALL: " << all_names_value << endl;
 
   return 0;
 }
